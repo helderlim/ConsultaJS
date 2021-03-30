6546f249df364a9b8a8357a8a313660b47ffdd47
helderlim <helderfelima16@gmail.com>
2021-03-30 14:59:34 -0300

    feat: comando CONTINUE

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-let funcionarios = [
-    {id: 1, nome: "César", habilitado: false},
-    {id: 2, nome: "Jéssica", habilitado: false},
-    {id: 3, nome: "Renan", habilitado: true},
-    {id: 4, nome: "Marlon", habilitado: false},
-    {id: 5, nome: "Ana", habilitado: false}
+let alunos = [
+    {id: 1, nome: "Bruna", media: 8},
+    {id: 2, nome: "Laura", media: 7},
+    {id: 3, nome: "José", media: 5},
+    {id: 4, nome: "Guilherme", media: 4},
+    {id: 5, nome: "Rafael", media: 10},
 ];
 
 let contador = 0;
-let encontrouHabilitado = false;
 
-while ( contador < funcionarios.length ) {
+while ( contador < alunos.length ) {
 
-    let funcionario = funcionarios[contador];
-
-    if( funcionario.habilitado == true ){
-        console.log("Funcionário habilitado encontrado: " + funcionario.nome);
-        encontrouHabilitado = true;
-        break;
-    }
+    let aluno = alunos[contador];
 
     contador++;
-}
 
-if (!encontrouHabilitado) {
-    console.log("Nenhum funcionário habilitado encontrado");
+    if ( aluno.media < 6 ) {
+        continue;
+    }
+
+    console.log("Id: " + aluno.id);
+    console.log("Nome: " + aluno.nome);
+    console.log("Média: " + aluno.media);
+    console.log("\n");
+
 }
